9e4c7a2d6cd6b3ee8b68ee71f68bcc199f1a7898
Gihan Alwis <ebi011@jithpl.com>
2025-07-17 12:13:29 +0530

    test

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -20,9 +20,9 @@ const AppHeader: React.FC = () => {
     }
   };
 
-  // const toggleApplicationMenu = () => {
-  //   setApplicationMenuOpen(!isApplicationMenuOpen);
-  // };
+  const toggleApplicationMenu = () => {
+    setApplicationMenuOpen(!isApplicationMenuOpen);
+  };
   const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
@@ -101,7 +101,7 @@ const AppHeader: React.FC = () => {
             />
           </Link>
 
-          {/* <button
+          <button
             onClick={toggleApplicationMenu}
             className="flex items-center justify-center w-10 h-10 text-gray-700 rounded-lg z-99999 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-800 lg:hidden"
           >
@@ -119,7 +119,7 @@ const AppHeader: React.FC = () => {
                 fill="currentColor"
               />
             </svg>
-          </button> */}
+          </button>
 
           {/* <div className="hidden lg:block">
             <form>
